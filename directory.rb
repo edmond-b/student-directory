08949fe8031bb9d556d2fd4ced2f9d8f66279a0f
EC2 Default User <ec2-user@ip-172-31-6-75.eu-west-2.compute.internal>
2020-04-07 18:34:35 +0000

    Now add students through input method

--- directory.rb
+++ directory.rb
@@ -11,6 +11,19 @@ students = [
   {name: "Joffrey Baratheon", cohort: :november},
   {name: "Norman Bates", cohort: :november}]
 
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finnish,just hit return twice"
+  students = []
+  name = gets.chomp
+  while !name.empty? 
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
+
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
@@ -24,6 +37,7 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
