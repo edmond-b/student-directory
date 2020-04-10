96c8e35a68ee45fcf62925badfcb1d6189c28172
EC2 Default User <ec2-user@ip-172-31-6-75.eu-west-2.compute.internal>
2020-04-10 13:46:41 +0000

    Main directory finished

--- directory.rb
+++ directory.rb
@@ -16,11 +16,11 @@ students = [
 def input_students
   puts "Please enter the names of the students"
   puts "To finnish,just hit return twice"
-  name = gets.chomp
+  name = STDIN.gets.chomp
   while !name.empty? 
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
   @students
 end
@@ -72,7 +72,7 @@ end
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -86,8 +86,8 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(",")
     @students << {name: name, cohort: cohort.to_sym}
@@ -95,4 +95,17 @@ def load_students
   file.close
 end
 
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} students from #{filename}"
+  else 
+    puts "Sorry #{filename} doesn't exist"
+    exit
+  end
+end
+
+try_load_students
 interactive_menu
